76e627f269887fc1d9547fa0da00595e920924d9
Alan Rivera <arivera@bsd220.org>
2023-10-31 10:03:09 -0500

    new

--- Pig/ContentView.swift
+++ Pig/ContentView.swift
@@ -7,19 +7,30 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ContentView: View{
+    @State var turnScore = 0
+    @State var gameScore = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.gray.opacity(0.7).ignoresSafeArea()
+            VStack {
+                Image("Pig").resizable().frame(width: 150, height: 150)
+                customText(text: "Pig")
+                customText(text: "Turn Score: \(turnScore)")
+                customText(text: "Game Score: \(gameScore)")
+                Spacer()
+            }
         }
-        .padding()
     }
 }
-
-struct ContentView_Previews: PreviewProvider {
+struct customText: View {
+    let text: String
+    var body: some View {
+        Text(text).font(Font.custom("Marker Felt",size:36))
+    }
+    
+}
+        struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
